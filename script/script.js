f55eb42f87beb0cb300bb8617af55f50dee5b7c6
Nicolas Santos <144915645+NicolasSantos19k@users.noreply.github.com>
2024-12-06 20:50:41 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,131 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'none';
+}
+/*scroll reveal*/
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.animation-1', {
+    duration: 1500,
+    distance: '90px',
+})
+revelar.reveal('.animation-2', {
+    duration: 1500,
+    distance: '90px',
+    delay: 200,
+})
+revelar.reveal('.animation-3', {
+    duration: 1500,
+    opacity: 0,
+})
+revelar.reveal('.animation-4', {
+    duration: 1500,
+    opacity: 0,
+    delay: 200,
+})
+revelar.reveal('.animation-5', {
+    duration: 1500,
+    opacity: 0,
+    delay: 400,
+})
+revelar.reveal('.animation-6', {
+    duration: 1500,
+    distance: '90px',
+})
+revelar.reveal('.animation-7', {
+    duration: 1500,
+    distance: '90px',
+    delay: 200,
+})
+revelar.reveal('.animation-8', {
+    duration: 1500,
+    distance: '90px',
+    delay: 400,
+})
+revelar.reveal('.animation-9', {
+    duration: 1500,
+    distance: '90px',
+    delay: 200,
+    delay: 600,
+})
+revelar.reveal('.animation-10', {
+    duration: 1500,
+    distance: '90px',
+    delay: 800,
+})
+revelar.reveal('.animation-11', {
+    duration: 1500,
+    origin: 'left',
+    distance: '90px',
+})
+revelar.reveal('.animation-12', {
+    duration: 1500,
+    distance: '90px',
+    origin: 'right',
+    delay: 200,
+})
+revelar.reveal('.animation-13', {
+    duration: 1500,
+    opacity: 0,
+})
+revelar.reveal('.animation-14', {
+    duration: 1500,
+    distance: '90px',
+})
+revelar.reveal('.animation-15', {
+    duration: 1500,
+    distance: '90px',
+    delay: 200,
+})
+revelar.reveal('.animation-16', {
+    duration: 1500,
+    distance: '90px',
+    delay: 400,
+})
+revelar.reveal('.animation-17', {
+    duration: 1500,
+    distance: '2000px',
+    origin: 'right',
+})
+revelar.reveal('.animation-18', {
+    duration: 1500,
+    distance: '90px',
+})
+revelar.reveal('.animation-19', {
+    duration: 1500,
+    distance: '90px',
+    delay: 200,
+})
+revelar.reveal('.animation-20', {
+    duration: 1500,
+    distance: '90px',
+    delay: 400,
+})
+revelar.reveal('.animation-21', {
+    duration: 1500,
+    distance: '90px',
+    delay: 600,
+})
+revelar.reveal('.animation-22', {
+    duration: 1500,
+    distance: '2000px',
+    origin: 'left',
+})
+revelar.reveal('.animation-23', {
+    duration: 1500,
+    distance: '90px',
+    delay: 600,
+})
+revelar.reveal('.animation-24', {
+    duration: 1500,
+    opacity: 0,
+})
+revelar.reveal('.animation-25', {
+    duration: 1500,
+    opacity: 0,
+})
